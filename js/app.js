281c3ccb2117b5d5d6a0c1a5ab5d411891899b41
Viperboya <viperboy_a@hotmail.co.uk>
2020-09-26 11:01:25 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 1 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".mainText", { x: "200%" }, { x: "0%", duration: 1,  ease: Back.easeOut.config(1.7),stagger: 0.5 }, "-=1");
